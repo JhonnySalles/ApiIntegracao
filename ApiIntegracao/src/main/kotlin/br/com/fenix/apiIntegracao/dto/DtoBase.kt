4ac17359387eeb66295b2052c8b982ddef6b3f08
ElPsy <jhonnysallesnoschag@hotmail.com>
2025-07-25 23:04:39 -0300

    Ajuste pra não apresentação dos link no post.

--- ApiIntegracao/src/main/kotlin/br/com/fenix/apiIntegracao/dto/DtoBase.kt
+++ ApiIntegracao/src/main/kotlin/br/com/fenix/apiIntegracao/dto/DtoBase.kt
@@ -1,8 +1,15 @@
 package br.com.fenix.apiIntegracao.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+import org.springframework.hateoas.Links
 import org.springframework.hateoas.RepresentationModel
 import java.io.Serializable
 
 abstract class DtoBase<ID>() : RepresentationModel<DtoBase<ID>>(), Dto<ID>, Serializable {
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    override fun getLinks(): Links {
+        return super.getLinks()
+    }
 
 }
